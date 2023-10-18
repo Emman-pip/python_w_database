73d48e36b426b13abe0f84e08cad83059b4b6f9f
emman-pip <parrenoemmanuel756@gmail.com>
2023-10-19 07:55:09 +0800

    Add the mainframes.

--- src/redo_main.py
+++ src/redo_main.py
@@ -7,13 +7,31 @@ customtkinter.set_default_color_theme("green")
 customtkinter.set_appearance_mode("dark")
 customtkinter.deactivate_automatic_dpi_awareness()
 
+padding = 10
+
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("1000x750")
+        # self.geometry("1000x750")
         self.title("Hospital Records")
-        # self.frm_left = customtkinter.CTkFrame(self, )
+        self.grid_rowconfigure(0, weight=1)  # configure grid system
+        self.grid_columnconfigure(0, weight=1)
+
+        self.frm_left = customtkinter.CTkFrame(self, border_color="dark_color")
+        self.frm_left.grid(
+            row=0, column=0, padx=padding, pady=padding, sticky="ns", columnspan=1
+        )
+        # self.frm_left.columnconfigure(0, weight=0)
+
+        self.frm_records = customtkinter.CTkFrame(self, border_color="dark_color")
+        # self.frm_records.columnconfigure(0, weight=1)
+        self.frm_records.grid(
+            row=0, column=1, padx=padding, pady=padding, sticky="nsew", columnspan=3
+        )
+
+        self.btn_someButton = customtkinter.CTkButton(self.frm_left, text="click me")
+        self.btn_someButton.grid(row=0, pady=padding, padx=padding)
 
 
 if __name__ == "__main__":
